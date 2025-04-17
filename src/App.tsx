1d1d19fdd36b096b185696e00cba973d203b05f8
Rafael Guerra <rafa99gm@hotmail.com>
2025-04-17 12:01:39 +0100

    [GDF-3] feat: Implemented Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@pages/Home/Home.tsx';
+import Login from '@pages/Login/Login.tsx';
 import Header from '@components/Header/Header.tsx';
 import Footer from '@components/Footer/Footer.tsx';
 import './styles/global.css';
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </main>
       <Footer />
