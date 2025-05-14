acc2f1b08c9f1f911839331d6d4dccb6acdd89bf
rafaelguerra <166007987+daRafael@users.noreply.github.com>
2025-05-14 01:20:41 +0100

    [GDF-3] feat: Implemented Login Page (#2)
    Co-authored-by: Tiago Costa <43713396+MrValraven@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@pages/Home/Home.tsx';
+import Login from '@pages/Login/Login.tsx';
 import Register from '@pages/Register/Register.tsx';
 import Header from '@components/Header/Header.tsx';
 import Footer from '@components/Footer/Footer.tsx';
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/reset-password' element={<ResetPassword />} />
           <Route path='/register' element={<Register />} />
         </Routes>
